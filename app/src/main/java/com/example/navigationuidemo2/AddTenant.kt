9144e10f633b4939ccf25ea8ac3f222a8b2e9314
Nicole Gone <131747232+nlcol3gon3@users.noreply.github.com>
2025-08-08 19:59:45 +0300

    Update AddTenant.kt

--- app/src/main/java/com/example/navigationuidemo2/AddTenant.kt
+++ app/src/main/java/com/example/navigationuidemo2/AddTenant.kt
@@ -2,81 +2,40 @@ package com.example.navigationuidemo2
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.navigationuidemo2.databinding.AddtenantActivityBinding
 
 class AddTenant : AppCompatActivity() {
+    private lateinit var binding: AddtenantActivityBinding
+    private val viewModel: TenantViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.addtenant_activity)
-        Log.d("LIFECYCLE", "AddTenantActivity - onCreate")
+        binding = DataBindingUtil.setContentView(this, R.layout.addtenant_activity)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
 
+        val fullName = binding.fullName
+        val unitNumber = binding.unitNumber
+        val fullRent = binding.fullRent
 
-        val btnBack = findViewById<Button>(R.id.btnBack)
+        binding.button.setOnClickListener {
+            val name = fullName.text.toString()
+            val unit = unitNumber.text.toString()
+            val rent = fullRent.text.toString()
+            viewModel.addTenant(name, unit, rent)
 
-        btnBack.setOnClickListener {
-            val totalTenants = 1
-            val totalRent = 5000
+            fullName.text.clear()
+            unitNumber.text.clear()
+            fullRent.text.clear()
+        }
 
+        binding.btnBack.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("TotalTenants",totalTenants)
-            intent.putExtra("TotalRent", totalRent)
             startActivity(intent)
+            finish()
         }
-
-
-        val tvDisplayTenantInfo = findViewById<TextView>(R.id.tvAddTenantInfo)
-        val btnAddTenant = findViewById<Button>(R.id.button)
-        val fullName = findViewById<EditText>(R.id.fullName)
-        val unitNumber = findViewById<EditText>(R.id.unitNumber)
-        val fullRent = findViewById<EditText>(R.id.fullRent)
-
-        btnAddTenant.setOnClickListener {
-
-            val etFullName = fullName.text.toString()
-            val etUnitNumber = unitNumber.text.toString()
-            val etFullRent = fullRent.text.toString()
-
-            val tenantInfo = "Name: $etFullName\nUnit: $etUnitNumber\nRent: $etFullRent\n\n"
-            tvDisplayTenantInfo.append(tenantInfo)
-        }
-
-
-
-
-    }
-    override fun onStart() {
-        super.onStart()
-        Log.d("LIFECYCLE", "SecondActivity - onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d("LIFECYCLE", "SecondActivity - onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("LIFECYCLE", "SecondActivity - onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("LIFECYCLE", "SecondActivity - onStop")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d("LIFECYCLE", "SecondActivity - onRestart")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("LIFECYCLE", "SecondActivity - onDestroy")
     }
 }
